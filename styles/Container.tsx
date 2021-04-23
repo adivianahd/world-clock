6d34bd5ed8bdbcbd6cfd380a9352e3ecbce1b1b9
adivianahd <adivianahd07@gmail.com>
2021-04-23 19:11:03 -0300

    create Container and create Layout

---
+++ styles/Container.tsx
@@ -0,0 +1,11 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+  display: flex;
+  aling-items: center;
+  justify-content: center;
+  width: 100%;
+  min-height: 110vh;
+  `
+
+export default Container
---
+++ styles/Layout.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components"
+
+const Layout = styled.div`
+  width: 100%;
+  min-width: 320px;
+  max-width: 1322px;
+  background: #f00;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  `
+
+export default Layout
