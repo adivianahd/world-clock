ac4961bf0e75ace18d91aba68291e6c54955905b
adivianahd <adivianahd07@gmail.com>
2021-04-24 22:42:07 -0300

    add service WorldTimeApiService

--- src/hooks/useWorldClock.tsx
+++ src/hooks/useWorldClock.tsx
@@ -1,31 +1,11 @@
 import { useState, createContext, useEffect, useContext } from "react";
 import Zone from "@type/Zone";
-
-const URL_API = "http://worldtimeapi.org/api/timezone/"
+import WorldTimeApiService from "@services/WorldTimeApiService";
 
 interface WorldClockValue { 
   options: string[]
   zonesSelected: Zone[]
   onSelect: (name: string) => void
-} 
-
-
-interface WorldTimeApiResponse {
-  abbreviation: string
-  client_ip: string
-  datetime: string
-  day_of_week: number
-  day_of_year: number
-  dst: false
-  dst_from: null
-  dst_offset: number
-  dst_until: null
-  raw_offset: number
-  timezone: string
-  unixtime: number
-  utc_datetime: string
-  utc_offset: string
-  week_number: number
 }
 
 const initialValues: WorldClockValue = {
@@ -40,30 +20,16 @@ interface WorldClockProviderProps {
   children: JSX.Element
 }
 
-const fetchData = (url: string) => fetch(url).then(response => response.json()).catch(() => {});
-
-const getZones = (): Promise<string[]>  => fetchData(URL_API)
-
-const getZoneByName = (name: string): Promise<WorldTimeApiResponse>  => fetchData(`${URL_API}${name}`)
-
-
 export function WorldClockProvider  ({ children }: WorldClockProviderProps)  {
   const [options, setOptions] = useState(initialValues.options)
   const [zonesSelected, setZonesSelected] = useState(initialValues.zonesSelected)
 
   useEffect(() => {
-    getZones().then(setOptions)
+    WorldTimeApiService.getZones().then(setOptions)
   },[])
 
   const onSelect = (name: string) => {
-
-    getZoneByName(name).then(({datetime}) => {
-      const newZoneSelected: Zone = {
-        name,
-        date: datetime,
-        hour:  datetime,
-      };
-
+    WorldTimeApiService.getZoneByName(name).then((newZoneSelected) => {
       setZonesSelected([newZoneSelected, ...zonesSelected])
     })
   }
---
+++ src/services/WorldTimeApiService.ts
@@ -0,0 +1,36 @@
+import Zone from "@type/Zone";
+
+const URL_API = "http://worldtimeapi.org/api/timezone/"
+
+interface WorldTimeApiZoneResponse {
+  abbreviation: string
+  client_ip: string
+  datetime: string
+  day_of_week: number
+  day_of_year: number
+  dst: false
+  dst_from: null
+  dst_offset: number
+  dst_until: null
+  raw_offset: number
+  timezone: string
+  unixtime: number
+  utc_datetime: string
+  utc_offset: string
+  week_number: number
+}
+
+const fetchData = (url: string) => fetch(url).then(response => response.json()).catch(() => {});
+
+const getZones = (): Promise<string[]>  => fetchData(URL_API);
+const getZoneByName = (name: string): Promise<Zone>  => fetchData(`${URL_API}${name}`)
+  .then(({datetime}: WorldTimeApiZoneResponse) => ({
+    name,
+    date: datetime,
+    hour:  datetime,
+  }))
+
+export default {
+  getZones,
+  getZoneByName
+}
