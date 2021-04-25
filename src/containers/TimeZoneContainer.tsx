12de08bb4ba0436902f942310cab6d04cbf3f394
adivianahd <adivianahd07@gmail.com>
2021-04-25 00:02:19 -0300

    add loader

--- src/containers/TimeZoneContainer.tsx
+++ src/containers/TimeZoneContainer.tsx
@@ -1,14 +1,16 @@
 import Layout from "@styles/Layout";
 import Container from "@styles/Container";
+import Loader from "@styles/Loader";
 import Input from "@components/Input";
 import GridClock from "@components/GridClock";
 import { useWorldClock } from "@hooks/useWorldClock";
 
 export default function TimeZoneContainer() {
-  const { options, zonesSelected, onSelect, onDelete } = useWorldClock();
+  const { options, zonesSelected, loading, onSelect, onDelete } = useWorldClock();
 
   return (
     <Container>
+      {loading && <Loader />}
       <Layout>
         <Input options={options} onSelect={onSelect} />
         <GridClock zonesSelected={zonesSelected} onDelete={onDelete} />
---
+++ src/styles/Loader.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const Loader = styled.div`
+  top: 0px;
+  bottom: 0px;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  background: rgba(150, 150, 150, .5);
+  z-index: 1;
+`
+export default Loader
